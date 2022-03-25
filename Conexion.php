fccef6ee613982c00be37c6178daf0ae6229ab8e
raso51 <101665818+raso51@users.noreply.github.com>
2022-03-25 15:49:14 -0500

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -5,19 +5,8 @@
 	<title>Conexion a BD</title>
 </head>
 <body>
-	<?php
-		//permite conectarse con el servidor local mediante el usuario root
-		$cn=mysqli_connect('localhost','root','');
-		//selecciona la base de datos de la conexion al servidor local
-		$bd=mysqli_select_db($cn,'bdvehiculo');
-		//verificar el estado de la conexion
-		if (($cn==true) && ($bd==true)) {
-			echo "Conexion Satisfactoria";
-		}else{
-			echo("error");
-			//permite la conexion
-			mysql_close($cn);
-	}
+	<?php 
+		echo("PHP en la nube")
 	?>
 </body>
 </html>
