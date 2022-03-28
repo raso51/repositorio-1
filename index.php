349635c2895b08d98e2aa85f76a2f928ad656ea5
raso51 <101665818+raso51@users.noreply.github.com>
2022-03-28 11:04:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,28 @@
 	<title>Conexion a BD</title>
 </head>
 <body>
-	<?php
-    echo("PHP en la nube ya funciona");
-	?>
+	<table border="3" class="table-1">
+		<tr>
+			<th>Nombres</th>
+			<td><input type="text" name="txtNombre" class="nombre-text">
+		</tr>
+		<tr>
+			<th>Apellidos</th>
+			<td><input type="text" name="txtApellido" class="apellido-text"></td>
+		</tr>
+		<tr>
+			<th>Edad</th>
+			<td><input type="number" name="txtEdad" class="edad-text"></td>
+		</tr>
+		<tr>
+			<th>DNI</th>
+			<td><input type="text" name="txtDni" class="dni-text"></td>
+		</tr>
+		<tr>
+			<th>Correo</th>
+			<td><input type="text" name="txtCorreo" class="correo-text"></td>
+		</tr>
+	</table>
+	<input type="submit" name="btnGuardar" value="Guardar" class="boton-guardar">
 </body>
 </html>
